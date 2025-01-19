8b85dce2db6551a088362ca3289393bbf2d87d26
emilyjoo <emilyp7370@gmail.com>
2025-01-19 19:55:41 +0900

    app_v2

--- app.py
+++ app.py
@@ -5,19 +5,17 @@ import re
 import time
 from datetime import timedelta
 from typing import Any
-
 from dotenv import load_dotenv
+from langchain.chains import create_history_aware_retriever
 from langchain_community.chat_message_histories import MomentoChatMessageHistory
+from langchain_community.vectorstores import Pinecone
 from langchain_core.callbacks import BaseCallbackHandler
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.outputs import LLMResult
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 from slack_bolt import App
 from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-
-
 
 CHAT_UPDATE_INTERVAL_SEC = 1
 
@@ -25,9 +23,7 @@ load_dotenv()
 
 # 로그
 SlackRequestHandler.clear_all_log_handlers()
-logging.basicConfig(
-    format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO
-)
+logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # 봇 토큰과 소켓 모드 핸들러를 사용하여 앱을 초기화
@@ -36,7 +32,6 @@ app = App(
     token=os.environ["SLACK_BOT_TOKEN"],
     process_before_response=True,
 )
-
 
 class SlackStreamingCallbackHandler(BaseCallbackHandler):
     last_send_time = time.time()
@@ -51,62 +46,33 @@ class SlackStreamingCallbackHandler(BaseCallbackHandler):
 
     def on_llm_new_token(self, token: str, **kwargs) -> None:
         self.message += token
-
         now = time.time()
         if now - self.last_send_time > self.interval:
             app.client.chat_update(
-                channel=self.channel, ts=self.ts, text=f"{self.message}\n\nTyping..."
+                channel=self.channel,
+                ts=self.ts,
+                text=f"{self.message}\n\nTyping...",
             )
             self.last_send_time = now
             self.update_count += 1
-
             # update_count가 현재의 업데이트 간격 X10보다 많아질 때마다 업데이트 간격을 2배로 늘림
             if self.update_count / 10 > self.interval:
                 self.interval = self.interval * 2
-        
-    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
-        message_context = "OpenAI API에서 생성되는 정보는 부정확하거나 부적절할 수 있으며, 우리의 견해를 나타내지 않습니다."
-        message_blocks = [
-            {"type": "section", "text": {"type": "mrkdwn", "text": self.message}},
-            {"type": "divider"},
-            {
-                "type": "context",
-                "elements": [{"type": "mrkdwn", "text": message_context}],
-            },
-        ]
+
+    def on_llm_end(self, response, **kwargs: Any) -> Any:
         app.client.chat_update(
             channel=self.channel,
             ts=self.ts,
             text=self.message,
-            blocks=message_blocks,
         )
 
+def initialize_vectorstore():
+    from langchain_openai import OpenAIEmbeddings
+    index_name = os.environ["PINECONE_INDEX"]
+    embeddings = OpenAIEmbeddings()
+    return Pinecone.from_existing_index(index_name, embeddings)
 
-# @app.event("app_mention")
-
-'''
-def handle_mention(event, say):
-    
-    channel = event["channel"]
-    thread_ts = event["ts"]
-    message = re.sub("<@.*>", "", event["text"])
-    #user = event["user"]
-    #say(thread_ts=thread_ts ,text=f"Hello <@{user}>!")
-
-    result = say("\n\nTyping...", thread_ts=thread_ts)
-    ts = result["ts"]
-
-    callback = SlackStreamingCallbackHandler(channel=channel, ts=ts)
-    
-    llm = ChatOpenAI(
-        model_name=os.environ["OPENAI_API_MODEL"],
-        temperature=os.environ["OPENAI_API_TEMPERATURE"],
-        streaming=True,
-        callbacks=[callback],
-    ) 
-    #response = llm.invoke(message)    
-    llm.invoke(message)    
-'''
+@app.event("app_mention")
 def handle_mention(event, say):
     channel = event["channel"]
     thread_ts = event["ts"]
@@ -126,49 +92,56 @@ def handle_mention(event, say):
         timedelta(hours=int(os.environ["MOMENTO_TTL"]))
     )
 
-    prompt = ChatPromptTemplate.from_messages(
+    vectorstore = initialize_vectorstore()
+    retriever = vectorstore.as_retriever()
+    rephrase_prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", "You are a good assistant."),
-            (MessagesPlaceholder(variable_name="chat_history")),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("user", "{input}"),
+            ("user", "위의 대화 내용을 바탕으로 적합한 검색 쿼리를 작성해 주세요."),
+        ]
+    )
+    rephrase_llm = ChatOpenAI(
+        model_name=os.environ["OPENAI_API_MODEL"],
+        temperature=os.environ["OPENAI_API_TEMPERATURE"],
+    )
+    rephrase_chain = create_history_aware_retriever(rephrase_llm, retriever, rephrase_prompt)
+
+    callback = SlackStreamingCallbackHandler(channel=channel, ts=ts)
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "아래 문맥만을 참고하여 답변해 주세요.\n\n{context}"),
+            MessagesPlaceholder(variable_name="chat_history"),
             ("user", "{input}"),
         ]
     )
-
-    callback = SlackStreamingCallbackHandler(channel=channel, ts=ts)
-    
-    llm = ChatOpenAI(
+    qa_llm = ChatOpenAI(
         model_name=os.environ["OPENAI_API_MODEL"],
         temperature=os.environ["OPENAI_API_TEMPERATURE"],
         streaming=True,
         callbacks=[callback],
     )
-    
-    chain = prompt | llm | StrOutputParser()
+    qa_chain = qa_prompt | qa_llm | StrOutputParser()
 
-    ai_message = chain.invoke({"input": message, "chat_history": history.messages})
-    
+    conversational_chain = (
+        qa_chain | rephrase_chain
+    )
+
+    ai_message = conversational_chain.invoke(
+        {"input": message, "chat_history": history.messages}
+    )
+
     history.add_user_message(message)
     history.add_ai_message(ai_message)
-
-
-def just_ack(ack):
-    ack()
-
-
-app.event("app_mention")(ack=just_ack, lazy=[handle_mention])
 
 # 소켓 모드 핸들러를 사용해 앱을 시작
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
 
-
 def handler(event, context):
     logger.info("handler called")
     header = event["headers"]
-    logger.info(json.dumps(header))
-
     if "x-slack-retry-num" in header:
-        logger.info("SKIP > x-slack-retry-num: %s", header["x-slack-retry-num"])
         return 200
  
     # AWS Lambda 환경의 요청 정보를 앱이 처리할 수 있도록 변환해 주는 어댑터
